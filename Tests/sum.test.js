67fc2e9612da7f78b165a046d3eabee454414c8f
LeandroOnline <leandrosavat@gmail.com>
2024-03-16 12:02:12 -0300

    feat: first pwa

--- Tests/sum.test.js
+++ Tests/sum.test.js
@@ -1,8 +1,9 @@
 import sum from "../Utils/sum";
+
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
 
 test("adds 2 + 2 to equal 4", () => {
-    expect(sum(2, 2)).toBe(4);
-  });
+  expect(sum(2, 2)).toBe(4);
+});
